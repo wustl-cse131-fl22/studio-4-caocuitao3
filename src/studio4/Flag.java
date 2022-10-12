14f6452897d095baaf8d628e41a250da544cd2b2
leah <leah@localhost>
2022-10-12 16:53:06 -0500

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.15, 0.5, 0.15, 0.5);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.45, 0.5, 0.15, 0.5);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.75, 0.5, 0.15, 0.5);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,57 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled=in.nextBoolean();
+		if(shape.equals("rectangle")) {
+			double x=in.nextDouble();
+			double y=in.nextDouble();
+			double halfWidths=in.nextDouble();
+			double halfHeight=in.nextDouble();
+			StdDraw.setPenColor(red, green, blue);
+			if (isFilled) {
+				
+				StdDraw.filledRectangle(x,y,halfWidths,halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x,y,halfWidths,halfHeight);
+			}
+		}
+		else if(shape.equals("ellipse")) {
+			double x=in.nextDouble();
+			double y=in.nextDouble();
+			double halfWidths=in.nextDouble();
+			double halfHeight=in.nextDouble();
+			StdDraw.setPenColor(red, green, blue);
+			if (isFilled) {
+				
+				StdDraw.filledEllipse(x,y,halfWidths,halfHeight);
+			}
+			else {
+				StdDraw.ellipse(x,y,halfWidths,halfHeight);
+			}
+		}
+		else if(shape.equals("triangle")) {
+			double[] x= new double[3];
+			double[] y=new double [3];
+			x[0]=in.nextDouble();
+			y[0]=in.nextDouble();
+			x[1]=in.nextDouble();
+			y[1]=in.nextDouble();
+			x[2]=in.nextDouble();
+			y[2]=in.nextDouble();
+			StdDraw.setPenColor(red, green, blue);
+			if (isFilled) {
+				
+				StdDraw.filledPolygon(x,y);
+			}
+			else {
+				StdDraw.polygon(x,y);
+			}
+		}
 		
 	}
 }
